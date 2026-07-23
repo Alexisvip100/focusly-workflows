f4042ad9bb1d63e1b69aa3d23c9e8d45290bd490
Alexisvip100 <alexisultreras01@gmail.com>
2026-07-24 04:07:59 +0800

    fix: add models

--- init_db.py
+++ init_db.py
@@ -6,7 +6,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from app.database import engine, Base
-from app.models import models  # Import models to register them on Base
+import app.models  # Import models to register them on Base
 
 async def init_db():
     print("Initializing database...")
